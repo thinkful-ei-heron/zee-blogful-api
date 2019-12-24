22e801aa95becc94dec2659f1c5b12760ec0bad4
Zee Celest <zeecelest@gmail.com>
2019-12-24 13:45:09 -0500

    preparing to deploy

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -10,7 +10,7 @@ describe('Bookmarks Endpoints', function() {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     });
     //db is the knexInstance
     app.set('db', db);
